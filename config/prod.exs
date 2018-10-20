0696d637d432c94cba7ead63ee75fd7afff2de6b
Jan Zimmermann <janzim23@gmail.com>
2018-10-20 19:21:46 -0400

    config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :task1, Task1Web.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "task1.jandev.org", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
